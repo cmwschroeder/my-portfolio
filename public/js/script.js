e2bba7e9ef091bd7cf3e19ba1814b1e36d2116cd
cmwschroeder <cmwschroeder@gmail.com>
2022-05-15 19:02:58 -0700

    added javascript to make artketplace gif play on mouseover

--- public/js/script.js
+++ public/js/script.js
@@ -4,18 +4,29 @@ var pethubImgEl = $('#pethub-image');
 var weatherDashEl = $('#weather-dashboard');
 var weatherDashImgEl = $('#weather-dashboard-image');
 
+var artketplaceEl = $('#artketplace');
+var artketplaceImgEl = $('#artketplace-image');
+
 pethubEl.on("mouseover", function() {
     pethubImgEl.attr("src", "/gifs/pethub.gif");
 });
 
 pethubEl.on("mouseout", function() {
     pethubImgEl.attr("src", "/images/pethub.png")
-})
+});
 
 weatherDashEl.on("mouseover", function() {
     weatherDashImgEl.attr("src", "/gifs/weather-dashboard.gif");
-})
+});
 
 weatherDashEl.on("mouseout", function() {
     weatherDashImgEl.attr("src", "/images/weather-dashboard.png");
-})
+});
+
+artketplaceEl.on("mouseover", function() {
+    artketplaceImgEl.attr("src", "/gifs/artketplace.gif");
+});
+
+artketplaceEl.on("mouseout", function() {
+    artketplaceImgEl.attr("src", "/images/artketplace.png");
+});
