9c9b7db1c3e56bcb66e925d6199bea2b8b4cc839
cmwschroeder <cmwschroeder@gmail.com>
2022-05-15 18:17:25 -0700

    fixed some html added route to render portfolio page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
     try {
-
+        res.render('homePage', { });
     } catch (err) {
-
+        res.status(500).json(err);
     }
 });
 
